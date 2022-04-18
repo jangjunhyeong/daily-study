c89c3094d285fd97445308c0328f66ff3699a5a3
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-04-18 16:45:27 +0900

    java연습

---
+++ 2022-04-18/java_practice/test.js
@@ -0,0 +1,23 @@
+let fruits = {
+	banana :5000,
+	apple : 3000,
+	kiwi : 1000,
+	cherry : 7000
+}
+
+console.log("이번 달 과일 지출비"+(fruits.banana*5+fruits.apple))
+
+let snacks = {
+	icecream : 1000,
+	cookies : 1500,
+	chocolate : 2200,
+	candy : 300
+}
+console.log("이번 달 간식 지출비"+(snacks.icecream*16+snacks.cookies*8+snacks.candy*5))
+
+let salad = {
+	chicory : 3000,
+	butterhead : 2000,
+	lettuce	: 3000,
+}
+console.log("이번 주 샐러드 지출비"+(salad.chicory+salad.butterhead*2+salad.lettuce*2))

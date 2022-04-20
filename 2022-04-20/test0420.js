08b0e92c9416e2aa2d870f6a169897dfb1a012a4
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-04-20 17:47:56 +0900

    script_test

---
+++ 2022-04-20/test0420.js
@@ -0,0 +1,21 @@
+// let sleepy = {
+// 	name : "sleepy",
+// 	gender :"male"
+// }
+// console.log(sleepy);
+// if(sleepy.name ==="junhyeong"){
+// 	console.log("미나미다");	{
+// 		console.log("틀렸어");
+// 	}
+// }
+
+
+let a = 1;
+let b = 2;
+let result = "값이 다릅니다."
+if(a===b){
+	console.log('값이 같습니다.');
+}else{
+		console.log(result);
+		// 거짓일 경우 번수인 result 를 기록하는 행동만 '할 뿐이다' 변수의 값이 무엇인지 관심이 없다.
+	}

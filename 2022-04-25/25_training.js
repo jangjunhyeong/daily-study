fc6db3ac0ded52fadb2f7d4afaa4578e2b854fe0
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-04-25 17:56:41 +0900

    script-practice

---
+++ 2022-04-25/25_training.js
@@ -0,0 +1,21 @@
+const a = 25;
+const b = 30;
+const c = 60;
+
+if (a+b===c) {
+
+console.log(`a와 b의 합은 c가 맞습니다!`);
+} else {
+	console.log(`a와 b의 합은 c가 아닙니다!`)
+}
+
+const e = 2;
+const f = 8;
+const g = 16;
+
+if (e*f===g){
+
+	console.log(`e에 f를 곱한 값은 g가 맞습니다!`);
+} else {
+	console.log(`e에 f를 곱한 값은 g가 아닙니다!`);
+}

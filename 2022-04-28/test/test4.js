5c46474d2b3ad51145c9b1e780b823256c8386ad
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-04-28 16:24:04 +0900

    update

---
+++ 2022-04-28/test/test4.js
@@ -0,0 +1,13 @@
+const obj = {
+	name:"dream",
+	work:["html","css","front-end","develop","database"],
+	count : 8,
+	basic: {
+		language : "javascript",
+		category : "programming"
+	}
+}
+
+
+console.log();
+
